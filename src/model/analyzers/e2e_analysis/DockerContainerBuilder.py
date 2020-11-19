88739d17be450f8b2bd27e94628623401ebae58d
Aziz Sonawalla <azizsona@students.cs.ubc.ca>
2020-11-18 20:03:11 -0500

    some more skeleton files

---
+++ src/model/analyzers/e2e_analysis/DockerContainerBuilder.py
@@ -0,0 +1,10 @@
+class DockerImageBuilder:
+
+    @staticmethod
+    def buildImage(program_file_path, command):
+        """
+
+        :param program_file_path:
+        :param command:
+        :return:
+        """
