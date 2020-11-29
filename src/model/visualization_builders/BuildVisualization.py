acbbd5fc9cf004c6174224eef189d92d1b6c0bb4
jw97 <jake.wong@alumni.ubc.ca>
2020-11-28 18:42:48 -0800

    add logging to build_vis

--- src/model/visualization_builders/BuildVisualization.py
+++ src/model/visualization_builders/BuildVisualization.py
@@ -6,6 +6,7 @@ from src.model.analyzers.e2e_analysis.result_types.InputSizeResult import InputS
 from src.model.data_transformers.ClosestFit import find_O_fit
 from src.model.data_transformers.ReferenceFits import FitData, get_reference_fits
 from src.model.data_transformers.SankeyDataCalculator import get_sankey_data
+from src.model.util.Logger import debug
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 CLIENT_DIR = os.path.abspath(os.path.join(CURRENT_DIR, "..", "..", "client"))
@@ -19,9 +20,11 @@ def build_visualization(program_file_path, profiler_results, e2e_results: Dict[i
     :param profiler_results: results from the profile analysis
     :param e2e_results: results from the end to end analysis
     """
+    debug("Starting visualization building")
     output = {"script_name": program_file_path}
 
     # create class object, two arrays for runtime and memory containing original file name, avg time/memory per class
+    debug("Creating class objects")
     class_runtime = []
     class_memory = []
     for c in profiler_results["class"]:
@@ -36,9 +39,11 @@ def build_visualization(program_file_path, profiler_results, e2e_results: Dict[i
             "percent_memory": c.memory_percentage_of_total
         })
     output["class"] = {"class_runtime": class_runtime, "class_memory": class_memory}
+    debug("Class objects created")
 
     # create function object, two arrays for runtime and memory containing original file name, avg time/memory per function
     # find function that takes longest/uses most memory
+    debug("Creating function objects")
     function_runtimes = []
     function_memory = []
     max_fun_runtime = 0.0
@@ -63,11 +68,21 @@ def build_visualization(program_file_path, profiler_results, e2e_results: Dict[i
             max_fun_memory_name = f.name
             max_fun_memory = f.total_memory
     output["function"] = {"function_runtime": function_runtimes, "function_memory": function_memory}
+    debug("Function objects created")
 
     # create array of line objects with filename, line#, code content, runtime, and memory usage
+    debug("Creating line objects")
     line_by_line = []
+    line_of_function = []
+    current_fun = ""
     for l in profiler_results["line_by_line"]:
-        line_by_line.append({
+        if current_fun == "":
+            current_fun = l.filename
+        else:
+            if current_fun != l.filename:
+                line_by_line.append(line_of_function)
+                line_of_function = []
+        line_of_function.append({
             "fileName": l.filename,
             "line_num": l.line_num,
             "code": l.line_text,
@@ -76,19 +91,25 @@ def build_visualization(program_file_path, profiler_results, e2e_results: Dict[i
             "percent_runtime": l.time_percentage_of_total,
             "percent_memory": l.memory_percentage_of_total
         })
+    line_by_line.append(line_of_function)
     output["line_by_line"] = line_by_line
+    debug("Line objects created")
 
     e2e_object = {}
     e2e_runtime = []
     e2e_memory = []
     # calculate fit line data for runtime & memory
+    debug("Calculating runtime fit lines")
     fit_data_runtime: FitData = get_reference_fits(e2e_results, True)
+    debug("Calculating memory fit lines")
     fit_data_memory: FitData = get_reference_fits(e2e_results, False)
     total_runtime_points = {}
     total_memory_points = {}
 
     # e2e object contains two arrays for runtime and memory containing the n parameter, avg runtime/memory, and associated fit data
+    debug("Creating e2e object")
     for i in e2e_results:
+        debug("Parsing e2e - n = ", i)
         e2e_runtime.append({
             "n": i,
             "total_runtime": round(e2e_results[i].average.total_runtime_ms, 2),
@@ -123,16 +144,22 @@ def build_visualization(program_file_path, profiler_results, e2e_results: Dict[i
     e2e_object["e2e_highest_memory_usage_function"] = max_fun_memory_name
     e2e_object["e2e_total_average_time"] = round(sum(list(total_runtime_points.values())) / len(e2e_runtime), 2)
     e2e_object["e2e_total_average_memory"] = round(sum(list(total_memory_points.values())) / len(e2e_memory), 2)
+    debug("Calculating project runtime complexity")
     e2e_object["e2e_time_complexity"] = find_O_fit(fit_data_runtime, total_runtime_points)
+    debug("Calculating project memory complexity")
     e2e_object["e2e_space_complexity"] = find_O_fit(fit_data_memory, total_memory_points)
     output["e2e"] = e2e_object
+    debug("e2e object created")
 
     #create sankey data object
+    debug("Creating sankey object")
     output["sankey"] = {
         "sankey_runtime": get_sankey_data(profiler_results, True),
         "sankey_memory": get_sankey_data(profiler_results, False)
     }
+    debug("Sankey object created")
 
     with open(DATA_FILE, 'w') as outfile:
         json.dump(output, outfile)
+    debug("JSON created")
     return output
